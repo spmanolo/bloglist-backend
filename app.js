2f108fd8e83cdb19728b7c99c0f0d7c10258c756
spmanolo <manolosan2001@gmail.com>
2023-10-13 15:20:50 +0200

    modificado app.js

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const config = require('./utils/config.js')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs.js')
-const { requestLogger, unknownEndpoint, errorHandler } = require('./utils/middlewares.js')
+const usersRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login.js')
+
+const errorHandler = require('./middlewares/errorHandler.js')
+const unknownEndpoint = require('./middlewares/unknownEndpoint.js')
+const requestLogger = require('./middlewares/requestLogger.js')
+
 const logger = require('./utils/logger.js')
 const mongoose = require('mongoose')
 
@@ -20,6 +27,8 @@ app.use(cors())
 app.use(requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
